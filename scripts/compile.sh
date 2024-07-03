8572796d2b1db77d089baf97130b3ff0b083c4d4
M-Arimase <arimase.zhao@gmail.com>
2024-07-04 03:13:55 +0800

    Added an implementation of the probability-based BKTree (PBKTree) using MinHash.
    1. Added an implementation of the probability-based BKTree (PBKTree) using MinHash: PBKTree includes two parameters:
       - num_tree: The number of BKTrees each barcode will be added to.
       - num_hash: The number of MinHashes used to cluster barcodes.
    
       The larger the num_hash, the smaller each cluster, the faster the lookup, but the higher the error rate; the larger the num_tree, the more lookups, the slower the lookup, but the lower the error rate. It is recommended to set min_tree=12 and min_hash=4.
    
    2. Improved the robustness of BKTree: Added a check to ensure the root node of the BKTree is not a null pointer.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ ./bktree/bktree.cpp -Ofast -march=native -shared -Wall -fPIC $(python -m pybind11 --includes) -o ./bktree/bktree$(python3-config --extension-suffix)
+g++ ./bktree/bktree.cpp -Ofast -march=native -shared -Wall -fPIC $(python -m pybind11 --includes) -o ./bktree/bktree$(python3-config --extension-suffix) -std=c++17
