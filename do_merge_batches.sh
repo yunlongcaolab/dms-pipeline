f0650030772036ef4a9be1066053be31eb114e8c
Fanchong Jian <jfc@pku.edu.cn>
2024-09-09 13:24:25 +0800

    Update sbatch script for general usage

--- do_merge_batches.sh
+++ do_merge_batches.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -e
+
+if [ -z $1 ]; then
+    echo "Please provide the project name as input!"
+    exit 1
+fi
+
 workdir=/lustre/grp/cyllab/share/DMS/$1
 
 python ext_utils/merge_escape_scores.py -i $workdir/processed -o $workdir/merge
